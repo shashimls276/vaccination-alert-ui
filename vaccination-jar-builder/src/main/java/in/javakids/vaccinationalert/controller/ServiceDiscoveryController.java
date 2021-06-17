ddb72dd72d09e96d0df8d9521ce2632259237336
ab68478 <ab68478@host.docker.internal>
2021-06-17 21:06:35 +0530

    commit

--- vaccination-jar-builder/src/main/java/in/javakids/vaccinationalert/controller/ServiceDiscoveryController.java
+++ vaccination-jar-builder/src/main/java/in/javakids/vaccinationalert/controller/ServiceDiscoveryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +18,14 @@ import in.javakids.vaccinationalert.model.ServiceDiscoveryVo;
 public class ServiceDiscoveryController {
 
 	@Autowired
+	@LoadBalanced
 	private DiscoveryClient discoveryClient;
 
 	@RequestMapping("/service-instances/{applicationName}")
 	public ServiceDiscoveryVo serviceInstancesByApplicationName(@PathVariable String applicationName) {
 		List<ServiceInstance> serviceInstances = this.discoveryClient.getInstances(applicationName);
+		List<String> services = this.discoveryClient.getServices();
+		System.out.println("Services :: "+services);
 		ServiceDiscoveryVo serviceDiscoveryVo = new ServiceDiscoveryVo();
 		
 		System.out.println("List : "+serviceInstances);
@@ -29,6 +33,9 @@ public class ServiceDiscoveryController {
 			System.out.println("Name :: "+s);
 			System.out.println(s.getInstanceId());
 			serviceDiscoveryVo.setUrl(s.getInstanceId());
+			if(!s.getInstanceId().contains("http://")) {
+				serviceDiscoveryVo.setUrl("http://"+s.getInstanceId());
+			}
 		}
 		return serviceDiscoveryVo;
 		
